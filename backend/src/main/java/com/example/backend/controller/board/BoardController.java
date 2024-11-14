a480f0e8480661677d898e4bd91e5e04158e0e59
Yun <y5624711@gmail.com>
2024-11-14 10:54:39 +0900

    boardController 추가

--- backend/src/main/java/com/example/backend/controller/board/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -5,9 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@ResponseBody
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
