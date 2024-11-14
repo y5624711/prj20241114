421f0ed3a9e0906477a29014dae236a1ebce7800
Yun <y5624711@gmail.com>
2024-11-14 10:39:44 +0900

    proxy 설정 /api -> localhost8080

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { "/api": { target: "http://localhost:8080" } } },
+  server: {
+    proxy: { "/api": { target: "http://localhost:8080" } },
+  },
 });
