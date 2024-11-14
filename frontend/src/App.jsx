902f9a90baf3fbd8349852145908088619ff8d91
Yun <y5624711@gmail.com>
2024-11-14 10:37:15 +0900

    글작성 form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,11 @@ import {
   RouterProvider,
   useNavigate,
 } from "react-router-dom";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import { Box, Flex, Input, Stack, Textarea } from "@chakra-ui/react";
+import { Field } from "./components/ui/field.jsx";
+import { Button } from "./components/ui/button.jsx";
+import { useState } from "react";
+import axios from "axios";
 
 function Navbar() {
   const navigate = useNavigate();
@@ -39,9 +43,38 @@ function BordList() {
 }
 
 function BoardAdd() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  const handleSaveClick = () => {
+    axios.post("/api/board/add", {
+      // 객체 프로퍼티명과 값의 이름이 같으면 생략해도됨
+      title,
+      content: content,
+      writer: writer,
+    });
+  };
   return (
     <Box>
       <h3>게시물 작성</h3>
+      <Stack gap={5}>
+        <Field label={"제목"}>
+          <Input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </Field>
+        <Field label={"본문"}>
+          <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Field>
+        <Field label={"작성자"}>
+          <Input value={writer} onChange={(e) => setWriter(e.target.value)} />
+        </Field>
+        <Box>
+          <Button onClick={handleSaveClick}>저장</Button>
+        </Box>
+      </Stack>
     </Box>
   );
 }
