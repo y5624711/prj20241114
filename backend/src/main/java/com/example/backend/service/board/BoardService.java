457a3ba3d8503e834d4a5b53a141ee7df3ba3ee0
Yun <y5624711@gmail.com>
2024-11-14 15:23:52 +0900

    글 작성 성공/실패 시 다른 기능 실행

--- backend/src/main/java/com/example/backend/service/board/BoardService.java
+++ backend/src/main/java/com/example/backend/service/board/BoardService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Transactional
@@ -15,16 +14,10 @@ import java.util.Map;
 public class BoardService {
     final BoardMapper mapper;
 
-    public Map<String, Object> add(Board board) {
+    public boolean add(Board board) {
         int cnt = mapper.insert(board);
 
-        if (cnt == 1) {
-            return Map.of("message",
-                    Map.of("type", "success", "text",
-                            board.getId() + "번 게시믈이 등록되었습니다."), "data", board);
-        } else {
-            return null;
-        }
+        return cnt == 1;
     }
 
     public List<Board> list() {
