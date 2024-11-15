ac1c453383c7d9d389b0a30f7a539c2beedbc088
Yun <y5624711@gmail.com>
2024-11-15 16:48:04 +0900

    아이디 확인 패스워드 확인 후 가입 버튼 활성화

--- frontend/src/page/member/MemberSignup.jsx
+++ frontend/src/page/member/MemberSignup.jsx
@@ -11,6 +11,7 @@ export function MemberSignup() {
   const [password, setPassword] = useState("");
   const [description, setDescription] = useState("");
   const [idCheck, setIdCheck] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState("");
   const navigate = useNavigate();
 
   function handleSaveClick() {
@@ -62,7 +63,11 @@ export function MemberSignup() {
 
   //가입버튼 비활성화 여부
   let disabled = true;
-  disabled = !idCheck;
+  if (idCheck) {
+    if (password === passwordCheck) {
+      disabled = !idCheck;
+    }
+  }
 
   return (
     <Box>
@@ -70,7 +75,13 @@ export function MemberSignup() {
       <Stack gap={5}>
         <Field label={"아이디"}>
           <Group attached w={"40%"}>
-            <Input value={id} onChange={(e) => setId(e.target.value)} />
+            <Input
+              value={id}
+              onChange={(e) => {
+                setIdCheck(false);
+                setId(e.target.value);
+              }}
+            />
             <Button onClick={handleIdCheckClick} variant={"outline"}>
               중복확인
             </Button>
@@ -80,6 +91,12 @@ export function MemberSignup() {
           <Input
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+          />
+        </Field>
+        <Field label={"암호확인"}>
+          <Input
+            value={passwordCheck}
+            onChange={(e) => setPasswordCheck(e.target.value)}
           />
         </Field>
         <Field label={"자기소개"}>
