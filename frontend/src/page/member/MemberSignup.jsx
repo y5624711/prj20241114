04a5cd03e8b861b4726666a6ebd59e1fd7a9f4ef
Yun <y5624711@gmail.com>
2024-11-15 16:40:56 +0900

    아이디 확인 후 가입버튼 활성화/비활성화

--- frontend/src/page/member/MemberSignup.jsx
+++ frontend/src/page/member/MemberSignup.jsx
@@ -10,6 +10,7 @@ export function MemberSignup() {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [description, setDescription] = useState("");
+  const [idCheck, setIdCheck] = useState(false);
   const navigate = useNavigate();
 
   function handleSaveClick() {
@@ -54,8 +55,15 @@ export function MemberSignup() {
           type: message.type,
           description: message.text,
         });
+
+        setIdCheck(data.available);
       });
   };
+
+  //가입버튼 비활성화 여부
+  let disabled = true;
+  disabled = !idCheck;
+
   return (
     <Box>
       <h3>회원가입</h3>
@@ -82,7 +90,9 @@ export function MemberSignup() {
         </Field>
 
         <Box>
-          <Button onClick={handleSaveClick}>가입</Button>
+          <Button disabled={disabled} onClick={handleSaveClick}>
+            가입
+          </Button>
         </Box>
       </Stack>
     </Box>
