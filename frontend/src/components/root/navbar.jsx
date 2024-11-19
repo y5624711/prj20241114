5ac828ccd8c6021ee5bef1125e7fe35855b6017f
Yun <y5624711@gmail.com>
2024-11-19 16:34:28 +0900

    navbar 에서 분해할당으로 코드 정리

--- frontend/src/components/root/navbar.jsx
+++ frontend/src/components/root/navbar.jsx
@@ -7,35 +7,33 @@ export function Navbar() {
   const navigate = useNavigate();
 
   // step 2 : context 사용하기
-  const authentication = useContext(AuthenticationContext);
+  const { id, isAdmin, isAuthenticated, logout } = useContext(
+    AuthenticationContext,
+  );
 
   return (
     <Flex gap={3}>
       <Box onClick={() => navigate("/")}>HOME</Box>
-      {authentication.isAuthenticated && (
-        <Box onClick={() => navigate("/add")}>작성</Box>
-      )}
-      {authentication.isAuthenticated || (
+      {isAuthenticated && <Box onClick={() => navigate("/add")}>작성</Box>}
+      {isAuthenticated || (
         <Box onClick={() => navigate("/member/signup")}>가입</Box>
       )}
-      {authentication.isAdmin && (
-        <Box onClick={() => navigate("/member/list")}>회원목록</Box>
-      )}
+      {isAdmin && <Box onClick={() => navigate("/member/list")}>회원목록</Box>}
 
-      {authentication.isAuthenticated || (
+      {isAuthenticated || (
         <Box onClick={() => navigate("/member/login")}>로그인</Box>
       )}
-      {authentication.isAuthenticated && (
+      {isAuthenticated && (
         <Box
           onClick={() => {
-            authentication.logout();
+            logout();
             navigate("/member/login");
           }}
         >
           로그아웃
         </Box>
       )}
-      <Box>{authentication.id}</Box>
+      <Box>{id}</Box>
     </Flex>
   );
 }
