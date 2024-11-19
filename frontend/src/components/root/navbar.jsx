400940d41a691b7ecdd787c3792e72491093611a
Yun <y5624711@gmail.com>
2024-11-19 14:05:40 +0900

    임시로 로그인한 사용자 출력

--- frontend/src/components/root/navbar.jsx
+++ frontend/src/components/root/navbar.jsx
@@ -1,8 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
+import { jwtDecode } from "jwt-decode";
 
 export function Navbar() {
   const navigate = useNavigate();
+
+  //TODO:임시
+  const token = localStorage.getItem("token");
+  let name;
+
+  if (token) {
+    const decode = jwtDecode(token);
+    name = decode.sub;
+  }
+
   return (
     <Flex gap={3}>
       <Box onClick={() => navigate("/")}>HOME</Box>
@@ -18,6 +29,7 @@ export function Navbar() {
       >
         로그아웃
       </Box>
+      <Box>{name}</Box>
     </Flex>
   );
 }
