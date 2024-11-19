6c924fdf8bd83c4d7acf9057f0e9c9b38ee165e5
Yun <y5624711@gmail.com>
2024-11-19 14:50:17 +0900

    admin만 회원목록 조회 가능

--- backend/src/main/java/com/example/backend/controller/member/MemberController.java
+++ backend/src/main/java/com/example/backend/controller/member/MemberController.java
@@ -72,6 +72,7 @@ public class MemberController {
 
 
     @GetMapping("list")
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
     public List<Member> list() {
         return service.list();
     }
