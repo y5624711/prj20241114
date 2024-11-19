2b04537efa5e3590dbe30a31007a43dace269692
Yun <y5624711@gmail.com>
2024-11-19 12:34:49 +0900

    로그아웃 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import axios from "axios";
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem("token");
 
-  if (!token) {
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
