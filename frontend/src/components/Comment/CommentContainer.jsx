1a3ac155659402eac55539a59669a6d9ae996b89
Yun <y5624711@gmail.com>
2024-11-20 11:37:39 +0900

    댓글 작성후 commentList update 됨 processing

--- frontend/src/components/Comment/CommentContainer.jsx
+++ frontend/src/components/Comment/CommentContainer.jsx
@@ -6,19 +6,25 @@ import axios from "axios";
 
 export function CommentContainer({ boardId }) {
   const [commentList, setCommentList] = useState([]);
+  const [processing, setProcessing] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(`/api/comment/list/${boardId}`)
-      .then((res) => res.data)
-      .then((data) => setCommentList(data));
-  }, []);
+    if (!processing) {
+      axios
+        .get(`/api/comment/list/${boardId}`)
+        .then((res) => res.data)
+        .then((data) => setCommentList(data));
+    }
+  }, [processing]);
 
   function handleSaveClick(comment) {
-    axios.post("/api/comment/add", {
-      boardId: boardId,
-      comment: comment,
-    });
+    setProcessing(true);
+    axios
+      .post("/api/comment/add", {
+        boardId: boardId,
+        comment: comment,
+      })
+      .finally(() => setProcessing(false));
   }
 
   return (
