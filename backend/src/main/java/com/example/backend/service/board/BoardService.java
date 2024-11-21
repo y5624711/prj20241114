366b3110a74dcbe2cacc4145574e1ff3f4e75fd4
Yun <y5624711@gmail.com>
2024-11-21 10:31:06 +0900

    파일 경로 앞부분을 설정파일에 작성

--- backend/src/main/java/com/example/backend/service/board/BoardService.java
+++ backend/src/main/java/com/example/backend/service/board/BoardService.java
@@ -4,6 +4,7 @@ import com.example.backend.dto.board.Board;
 import com.example.backend.dto.board.BoardFile;
 import com.example.backend.mapper.board.BoardMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,9 @@ import java.util.Map;
 public class BoardService {
 
     final BoardMapper mapper;
+
+    @Value("${image.src.prefix}")
+    String imageSrcPrefix;
 
     public boolean add(Board board, MultipartFile[] files, Authentication authentication) {
 
@@ -74,7 +78,7 @@ public class BoardService {
         Board board = mapper.selectById(id);
         List<String> fileNameList = mapper.selectFilesByBoardId(id);
         List<BoardFile> fileSrcList = fileNameList.stream()
-                .map(name -> new BoardFile(name, STR."http://172.30.1.49:8081/\{id}/\{name}")).toList();
+                .map(name -> new BoardFile(name, STR."\{imageSrcPrefix}/\{id}/\{name}")).toList();
         //http://172.30.1.49:8081/id/
         board.setFileList(fileSrcList);
         return board;
