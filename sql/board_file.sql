08c8ae9b871733a7694f97e46a06b7ba0202f681
Yun <y5624711@gmail.com>
2024-11-21 10:42:34 +0900

    게시물 목록 조회시 파일 수 계산 쿼리

--- sql/board_file.sql
+++ sql/board_file.sql
@@ -7,3 +7,20 @@ CREATE TABLE board_file
     PRIMARY KEY (board_id, name)
 );
 
+#게시믈 목록 조회(w/파일수)
+SELECT b.id, b.title, COUNT(DISTINCT c.id) 댓글수, COUNT(DISTINCT f.name) 파일수
+FROM board b
+         LEFT JOIN comment c
+                   ON b.id = c.board_id
+         LEFT JOIN board_file f
+                   ON b.id = f.board_id
+GROUP BY b.id
+ORDER BY b.id DESC;
+
+SELECT b.id,
+       b.title,
+       (SELECT COUNT(*) FROM comment c WHERE c.board_id = b.id)    댓글수,
+       (SELECT COUNT(*) FROM board_file f WHERE f.board_id = b.id) 파일수
+FROM board b
+GROUP BY b.id
+ORDER BY b.id DESC;
